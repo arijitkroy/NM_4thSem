2c269379413b79899f9a4c9c8669a61381cee2e9
Arijit Kumar Roy <83859073+arijitkroy@users.noreply.github.com>
2025-05-04 21:24:31 +0530

    Update NewtonForwardInterpolation.c

--- NewtonForwardInterpolation.c
+++ NewtonForwardInterpolation.c
@@ -7,36 +7,37 @@ long long fact(int x) {
     return f;
 }
 
-void displayTable(float x[SIZE], float table[SIZE][SIZE], int n) {
+float forward_interpolation(float x[], float y[], int n, float v) {
+    float table[SIZE][SIZE], result, u, h, u_term = 1;
     int i, j;
+    for (i = 0; i < n; i++) table[i][0] = y[i];
+    for (j = 1; j < n; j++)
+        for (i = 0; i < n - j; i++) 
+            table[i][j] = table[i+1][j-1] - table[i][j-1];
     for (i = 0; i < n; i++) {
-        printf("%0.2f\t", x[i]);
-        for (j = 0; j < n - i; j++) {
-            printf("%0.4f\t", table[j][i]);
-        }
+        printf("%.4f", table[i][0]);
+        for (j = 1; j < n - i; j++)
+            printf("\t%.4f", table[i][j]);
         printf("\n");
     }
+    h = x[1] - x[0];
+    u = (v - x[0]) / h;
+    result = table[0][0];
+    for (i = 1; i < n; i++) {
+        u_term *= (u + i - 1);
+        result += u_term * table[0][i] / fact(i);
+    }
+    return result;
 }
 
 void main() {
-    int i, j, n; 
-    float x[SIZE], table[SIZE][SIZE], h, u, res, v, term = 1;
-    printf("Number of datas: ");
+    float x[SIZE], sinx[SIZE], value;
+    int i, n;
+    printf("Number of data: ");
     scanf("%d", &n);
-    printf("Enter x & y:\n");
-    for (i = 0; i < n; i++) scanf("%f %f", &x[i], &table[0][i]);
-    printf("Enter value to find: ");
-    scanf("%f", &v);
-    for (i = 1; i < n; i++) 
-        for (j = 0; j < n - i; j++) 
-            table[i][j] = table[i-1][j+1] - table[i-1][j];
-    displayTable(x, table, n);
-    h = x[1] - x[0];
-    u = (v - x[0]) / h;
-    res = table[0][0];
-    for (i = 1; i < n; i++) {
-        term *= (u - (i - 1));
-        res += (u * table[i][0]) / fact(i);
-    }
-    printf("Interpolated Result: %0.3f", res);
+    printf("Enter x and sinx:\n");
+    for (i = 0; i < n; i++) scanf("%f %f", &x[i], &sinx[i]);
+    printf("Value to find: ");
+    scanf("%f", &value);
+    printf("Result: %0.3f\n", forward_interpolation(x, sinx, n, value));
 }
