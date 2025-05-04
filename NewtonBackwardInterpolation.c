4cc7e5a348a6df4fa1930092e4bb6c2227c298aa
Arijit Kumar Roy <83859073+arijitkroy@users.noreply.github.com>
2025-05-04 21:24:45 +0530

    Update NewtonBackwardInterpolation.c

--- NewtonBackwardInterpolation.c
+++ NewtonBackwardInterpolation.c
@@ -7,36 +7,37 @@ long long fact(int x) {
     return f;
 }
 
-void displayTable(float x[SIZE], float table[SIZE][SIZE], int n) {
+float backward_interpolation(float x[], float y[], int n, float v) {
+    float table[SIZE][SIZE], h, u, u_term = 1, result;
     int i, j;
+    for (i = 0; i < n; i++) table[i][0] = y[i];
+    for (j = 1; j < n; j++)
+        for (i = n - 1; i >= j; i--)
+            table[i][j] = table[i][j-1] - table[i-1][j-1];
     for (i = 0; i < n; i++) {
-        printf("%0.2f\t", x[i]);
-        for (j = 0; j <= i; j++) {
-            printf("%0.4f\t", table[j][i]);
-        }
+        printf("%0.4f", table[i][0]);
+        for (j = 1; j <= i; j++)
+            printf("\t%0.4f", table[i][j]);
         printf("\n");
     }
+    h = x[1] - x[0];
+    u = (v - x[n-1]) / h;
+    result = table[n-1][0];
+    for (i = 1; i < n; i++) {
+        u_term *= (u + i - 1);
+        result += u_term * table[n-1][i] / fact(i);
+    }
+    return result;
 }
 
 void main() {
-    int i, j, n;
-    float x[SIZE], table[SIZE][SIZE], h, u, term = 1, res, v;
-    printf("Number of datas: ");
+    float x[SIZE], y[SIZE], value;
+    int i, n;
+    printf("Number of data: ");
     scanf("%d", &n);
-    printf("Enter x & y:\n");
-    for (i = 0; i < n; i++) scanf("%f %f", &x[i], &table[0][i]);
-    for (i = 1; i < n; i++)
-        for (j = n - 1; j >= i; j--)
-            table[i][j] = table[i-1][j] - table[i-1][j-1];
-    printf("Enter value to find: ");
-    scanf("%f", &v);
-    displayTable(x, table, n);
-    h = x[n-1] - x[n-2];
-    u = (v - x[n-1]) / h;
-    res = table[0][n-1];
-    for (i = 1; i < n; i++) {
-        term *= (u + (i - 1));
-        res += (term * table[i][n-1]) / fact(i);
-    }
-    printf("Interpolated value: %0.3f", res);
+    printf("Enter x and y:\n");
+    for (i = 0; i < n; i++) scanf("%f %f", &x[i], &y[i]);
+    printf("Value to find: ");
+    scanf("%f", &value);
+    printf("Result: %0.3f\n", backward_interpolation(x, y, n, value));
 }
